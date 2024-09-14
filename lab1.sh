3ff92b135600676f5607317b0a44dc0b55c117ca
Eternal <124017017+Kevanko@users.noreply.github.com>
2024-09-14 12:28:48 +0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -35,32 +35,32 @@ free -h
 # 4. Параметры сетевых интерфейсов и скорость соединения
 echo "=== Параметры сетевых интерфейсов ==="
 ip addr | awk '
-/^[0-9]+:/ {
-    iface=$2; gsub(":", "", iface);
-    print "Интерфейс: " iface
-}
-/link\/ether/ {
-    print "MAC-адрес: " $2
-}
-/inet / {
-    print "IP-адрес: " $2
-    print ""
+#!/bin/bash
+
+# Вывод информации о сетевых интерфейсах
+ip -o link show | awk '
+{
+    iface=$2
+    if (iface ~ /:/) iface=substr(iface, 1, length(iface)-1)
+    cmd="ip addr show " iface
+    cmd | getline addr
+    close(cmd)
+    split(addr, a, " ")
+    ip_addr=a[3]
+    
+    cmd="ethtool " iface
+    cmd | getline speed
+    close(cmd)
+    split(speed, s, " ")
+    speed=s[2]
+    
+    cmd="cat /sys/class/net/" iface "/address"
+    cmd | getline mac_addr
+    close(cmd)
+    
+    printf "Интерфейс: %-10s IP: %-15s MAC: %-17s Скорость: %-10s\n", iface, ip_addr, mac_addr, speed
 }'
-# 4.1. Получение скорости соединения для каждого интерфейса
-for iface in $(ls /sys/class/net/); do
-  echo "Интерфейс: $iface"
 
-  # Используем ethtool для проводных интерфейсов
-  if command -v ethtool &> /dev/null; then
-    ethtool $iface 2>/dev/null | grep -i speed
-  fi
-
-  # Для беспроводных интерфейсов используем iwconfig
-  if command -v iwconfig &> /dev/null; then
-    iwconfig $iface 2>/dev/null | grep -i 'Bit Rate'
-  fi
-  echo ""
-done
 # 5. Информация о системных разделах
 print_section "Информация о файловых системах"
 df -h
